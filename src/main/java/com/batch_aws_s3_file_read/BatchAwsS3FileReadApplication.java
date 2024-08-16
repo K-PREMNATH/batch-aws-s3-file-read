b67bb8c954e963281128988fc5c12a665c5e9afc
Premnath <rexprem1991@gmail.com>
2024-08-16 14:33:14 +0800

    initial version committed

---
+++ src/main/java/com/batch_aws_s3_file_read/BatchAwsS3FileReadApplication.java
@@ -0,0 +1,13 @@
+package com.batch_aws_s3_file_read;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchAwsS3FileReadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchAwsS3FileReadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/batch_aws_s3_file_read/BatchAwsS3FileReadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batch_aws_s3_file_read;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchAwsS3FileReadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
